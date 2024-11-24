a9138d7da7064d04f278ff2d610e8da036c69455
Arif Shahriyar Nader <arif.nader95@gmail.com>
2024-11-24 23:15:41 +0600

    database added

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
 const express=require('express');
 const app=express();
 const bodyParser=require('body-parser');
+const connectDB=require('./config/db');
 
 //parse requests
 app.use(bodyParser.json());
+
+//db
+connectDB()
 
 const port=5000;
 app.listen(port, () =>{
