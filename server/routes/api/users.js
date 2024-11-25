c78fd5b352c3104aaaa0479d0bbcfb7ecd20d49b
Arif Shahriyar Nader <arif.nader95@gmail.com>
2024-11-26 00:57:11 +0600

    user login

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,72 +1,128 @@
-const express=require('express');
-const router=express.Router();
-const User=require('../../models/User');
-const bcrypt=require('bcrypt');
+const express = require("express");
+const router = express.Router();
+const User = require("../../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //register
-router.post('/register', async(req,res) =>{
-    try{
-        const salt= await bcrypt.genSalt(10);
-        const password=await bcrypt.hash(req.body.password, salt);
-        const userObj={
-            fname:req.body.fname,
-            lname:req.body.lname,
-            email:req.body.email,
-            password:password,
-            userType:req?.body?.userType || 'customer'
-        }
-        const user= new User(userObj);
-        await user.save()
-        return res.status(201).json(user);
-    }
-    catch(error){
-        res.status(500).json({message:'Something went wrong'})
-    }
-})
-//get all user
-router.get('/', async(req,res) =>{
-   try{
-        const users= await User.find();
-        res.status(200).json(users);
-   }
-   catch(error){
-    res.status(500).json({message:"Something went wrong"})
-   }
-})
-//update user
-router.put('/:id', async(req,res) =>{
-    try{
-        const id=req.params.id;
-        const userBody=req.body;
-        const updateUser=await User.findByIdAndUpdate(id, userBody,{
-            new:true,
-        })
-        if(updateUser){
-            return res.status(200).json(updateUser)
-        }
-        else{
-            return res.status(404).json({message:"user not found"})
-        }
-    }
-    catch(error){
-        res.status(500).json({message:"Something went wrong"})
-    }
-})
-//delete user
-router.delete('/:id', async(req,res) =>{
-    try{
-        const id=req.params.id;
-        const deleteUser= await User.findByIdAndDelete(id);
-        if(deleteUser){
-            res.json({message:"User is deleted"})
-        }
-        else{
-            res.status(404).json({message:"User not found"})
-        }
-    }
-    catch(error){
-        res.status(500).json({message:"Something went wrong"})
-    }
-})
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
+    const userObj = {
+      fname: req.body.fname,
+      lname: req.body.lname,
+      email: req.body.email,
+      password: password,
+      userType: req?.body?.userType || "customer",
+    };
+    const user = new User(userObj);
+    await user.save();
+    return res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
 
-module.exports=router;
+//user login
+router.post("/login", async (req, res) => {
+  try {
+    const { type, email, password, refresh } = req.body;
+    if (type === "email") {
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        res.status(500).json({ message: "User not found" });
+      }
+      await handleEmailLogin({ password, user, res });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+//get all user
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+//update user
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userBody = req.body;
+    const updateUser = await User.findByIdAndUpdate(id, userBody, {
+      new: true,
+    });
+    if (updateUser) {
+      return res.status(200).json(updateUser);
+    } else {
+      return res.status(404).json({ message: "user not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+//delete user
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deleteUser = await User.findByIdAndDelete(id);
+    if (deleteUser) {
+      res.json({ message: "User is deleted" });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+module.exports = router;
+
+async function handleEmailLogin({ password, user, res }) {
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (isValidPassword) {
+    const userObj = await generateUserObject(user);
+    return res.json(userObj);
+  } else {
+    res.status(401).json({ message: "Unable to login" });
+  }
+}
+function generateUserObject(user) {
+  const { accessToken, refreshToken } = generateToken(user);
+  const userObj = user.toJSON();
+  userObj["accessToken"] = accessToken;
+  userObj["refreshToken"] = refreshToken;
+  return userObj;
+}
+
+function generateToken(user) {
+  const accessToken = jwt.sign(
+    {
+      email: user.email,
+      _id: user._id,
+      userType: user.userType,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1d",
+    }
+  );
+  const refreshToken = jwt.sign(
+    {
+      email: user.email,
+      _id: user._id,
+      userType: user.userType,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "30d",
+    }
+  );
+
+  return { accessToken, refreshToken };
+}
