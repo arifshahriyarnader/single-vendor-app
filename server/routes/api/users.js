38511f53eed44ee069563fd94e18ff0fb533ab06
Arif Shahriyar Nader <arif.nader95@gmail.com>
2024-11-27 23:33:03 +0600

    user login by refresh token

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require("../../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const authenticateToken=require('../../middleware/auth');
+const authenticateToken = require("../../middleware/auth");
 
 //register
 router.post("/register", async (req, res) => {
@@ -28,13 +28,21 @@ router.post("/register", async (req, res) => {
 //user login
 router.post("/login", async (req, res) => {
   try {
-    const { type, email, password, refresh } = req.body;
+    const { type, email, password, refreshToken } = req.body;
     if (type === "email") {
       const user = await User.findOne({ email: email });
       if (!user) {
         res.status(500).json({ message: "User not found" });
       }
       await handleEmailLogin({ password, user, res });
+    } else if (type === "refresh") {
+      if (!refreshToken) {
+        return res.status(401).json({ message: "Refresh token is not found" });
+      } else {
+        await handleRefreshToken({ refreshToken, res });
+      }
+    } else {
+      return res.status(400).json({ message: "Invalid login type" });
     }
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
@@ -42,21 +50,19 @@ router.post("/login", async (req, res) => {
 });
 
 //user profile
-router.get('/user-profile', authenticateToken, async(req,res)  =>{
-  try{
-    const id=req.user._id;
-    const user= await User.findById(id)
-    if(user){
-      return res.json(user)
+router.get("/user-profile", authenticateToken, async (req, res) => {
+  try {
+    const id = req.user._id;
+    const user = await User.findById(id);
+    if (user) {
+      return res.json(user);
+    } else {
+      return res.status(500).json({ message: "User not found" });
     }
-    else{
-      return res.status(500).json({message:"User not found"})
-    }
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
   }
-  catch(error){
-    res.status(500).json({message:"Something went wrong"});
-  }
-})
+});
 
 //get all user
 router.get("/", async (req, res) => {
@@ -143,4 +149,20 @@ function generateToken(user) {
   );
 
   return { accessToken, refreshToken };
+}
+
+function handleRefreshToken({ refreshToken, res }) {
+  jwt.verify(refreshToken, process.env.JWT_SECRET, async (err, payload) => {
+    if (err) {
+      return res.status(404).json({ message: "Unauthorized" });
+    } else {
+      const user = await User.findById(payload._id);
+      if (user) {
+        const userObj = generateUserObject(user);
+        return res.status(200).json(userObj);
+      } else {
+        res.status(404).json({ message: "Unauthorized" });
+      }
+    }
+  });
 }
