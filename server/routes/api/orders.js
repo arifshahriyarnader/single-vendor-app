c23036b8c8ebf81ede30656503f68f267507a683
Arif Shahriyar Nader <arif.nader95@gmail.com>
2024-11-30 21:14:50 +0600

    upload a new file added

--- server/routes/api/orders.js
+++ server/routes/api/orders.js
@@ -3,4 +3,5 @@ const router=express.Router();
 const Order=require('../../models/Order');
 const authenticateToken=require('../../middleware/auth');
 
+
 module.exports=router;
--- server/routes/api/products.js
+++ server/routes/api/products.js
@@ -3,5 +3,40 @@ const router=express.Router();
 const File=require('../../models/File');
 const Product=require('../../models/Product');
 const authenticateToken=require('../../middleware/auth');
+const multer=require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix + "-" + file.originalname)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+
+//Upload a file
+router.post('/uploads', [authenticateToken, upload.single('file')], async(req,res) =>{
+    try{
+        if(req.user.userType != 'admin'){
+            res.status(401).josn({message:"You are not an admin"})
+        }
+        else{
+            const fileObj={
+                name:req.file.filename,
+                path:req.file.path
+            }
+
+            const file= new File(fileObj);
+            await file.save();
+            return res.json(file);
+        }
+    }
+    catch(error){
+        res.status(501).json({message:"Something went wrong"})
+    }
+})
 
 module.exports=router;
