4b2dd39f9248a542d08c00dacb268d52c7474f64
Arif Shahriyar Nader <arif.nader95@gmail.com>
2024-12-03 14:10:27 +0600

    create an order

--- server/routes/api/orders.js
+++ server/routes/api/orders.js
@@ -1,7 +1,52 @@
-const express=require('express');
-const router=express.Router();
-const Order=require('../../models/Order');
-const authenticateToken=require('../../middleware/auth');
+const express = require("express");
+const router = express.Router();
+const Order = require("../../models/Order");
+const authenticateToken = require("../../middleware/auth");
+const { body, validationResult } = require("express-validator");
+const Product = require("../../models/Product");
 
+//create an order
+router.post(
+  "/",
+  [
+    authenticateToken,
+    body("deliveryLocation", "deliveryLocation is required").notEmpty(),
+    body("qty", "qty requirements not met").notEmpty().isInt({ min: 1 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      } else {
+        const userId = req.user._id;
+        const productId = req.body.productId;
+        const product = await Product.findById(productId);
+        if (!product) {
+          return res.status(404).json({ message: "product not found" });
+        } else {
+          const now = new Date();
+          const orderObj = {
+            productId: productId,
+            userId: userId,
+            qty: parseInt(req.body.qty) || 1,
+            purchaseDate: now,
+            expectedDeliveryDate: now.setDate(now.getDate() + 5),
+            deliveryLocation: req.body.deliveryLocation,
+            deliveryStatus: "in-progress",
+          };
+          orderObj.total = orderObj.qty * product.price;
 
-module.exports=router;
+          const order = new Order(orderObj);
+          await order.save();
+          await order.populate(["productId", "userId"]);
+          return res.status(201).json(order);
+        }
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+);
+
+module.exports = router;
